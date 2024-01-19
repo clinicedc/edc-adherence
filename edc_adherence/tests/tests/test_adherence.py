23558b0ebc95b73c6a4414844691224618fce796
erikvw <ew2789@gmail.com>
2024-01-19 08:37:11 -0600

    fix tests

--- edc_adherence/tests/tests/test_adherence.py
+++ edc_adherence/tests/tests/test_adherence.py
@@ -1,8 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, tag
 from edc_appointment.models import Appointment
 from edc_appointment.tests.helper import Helper
-from edc_consent.site_consents import AlreadyRegistered
-from edc_consent.tests.consent_test_utils import consent_definition_factory
+from edc_consent import site_consents
 from edc_constants.constants import NEVER, NO, OTHER, YES
 from edc_facility import import_holidays
 from edc_list_data import site_list_data
@@ -12,33 +11,34 @@ from edc_adherence import list_data
 from edc_adherence.models import NonAdherenceReasons
 from edc_adherence.tests.admin import MedicationAdherenceAdmin, my_admin_site
 
+from ..consents import consent_v1
 from ..forms import MedicationAdherenceForm
 from ..models import MedicationAdherence, SubjectVisit
 from ..visit_schedules import visit_schedule
 
 
 class TestAdherence(TestCase):
+    helper_cls = Helper
+
     @classmethod
     def setUpTestData(cls):
         import_holidays()
 
     def setUp(self) -> None:
+        site_consents.registry = {}
+        site_consents.register(consent_v1)
         site_list_data.initialize()
         site_list_data.register(list_data, app_name="edc_adherence")
         site_list_data.load_data()
         site_visit_schedules._registry = {}
         site_visit_schedules.register(visit_schedule)
-        self.subject_identifier = "1234"
-        for schedule in visit_schedule.schedules.values():
-            try:
-                consent_definition_factory(model=schedule.consent_model)
-            except AlreadyRegistered:
-                pass
-        self.helper = Helper(subject_identifier=self.subject_identifier)
+        self.subject_identifier = "101-123400-0"
+        self.helper = self.helper_cls(
+            subject_identifier=self.subject_identifier,
+        )
         self.helper.consent_and_put_on_schedule(
             visit_schedule_name="visit_schedule",
             schedule_name="schedule",
-            age_in_years=25,
         )
         appointments = Appointment.objects.filter(subject_identifier=self.subject_identifier)
         self.subject_visit = SubjectVisit.objects.create(appointment=appointments[0])
@@ -109,6 +109,7 @@ class TestAdherence(TestCase):
         my_admin_site.register(MedicationAdherence, MedicationAdherenceAdmin)
         MedicationAdherenceAdmin(MedicationAdherence, my_admin_site)
 
+    @tag("1")
     def test_pill_count(self):
         data = dict(
             visual_score_slider=90,
