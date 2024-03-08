bbef4624c7a02d93c833e388feae71fe3d84c308
erikvw <ew2789@gmail.com>
2024-03-08 10:46:07 -0600

    change fieldset var to func

--- edc_adherence/model_admin_mixin.py
+++ edc_adherence/model_admin_mixin.py
@@ -4,39 +4,46 @@ from django.utils.html import format_html
 from django.utils.translation import gettext as _
 from django_audit_fields.admin import audit_fieldset_tuple
 
-missed_medications_fieldset_tuple = (
-    _("Missed Medications"),
-    {
-        "description": format_html(
-            render_to_string("edc_adherence/missed_medication_fieldset_description.html")
-        ),
-        "fields": (
-            "last_missed_pill",
-            "missed_pill_reason",
-            "other_missed_pill_reason",
-        ),
-    },
-)
 
-pill_count_fieldset_tuple = (
-    _("Pill Count"),
-    {
-        "fields": ("pill_count_performed", "pill_count", "pill_count_not_performed_reason"),
-    },
-)
+def get_missed_medications_fieldset_tuple():
+    description = render_to_string("edc_adherence/missed_medication_fieldset_description.html")
+    return (
+        _("Missed Medications"),
+        {
+            "description": format_html(description),
+            "fields": (
+                "last_missed_pill",
+                "missed_pill_reason",
+                "other_missed_pill_reason",
+            ),
+        },
+    )
+
+
+def get_pill_count_fieldset_tuple():
+    return (
+        _("Pill Count"),
+        {
+            "fields": (
+                "pill_count_performed",
+                "pill_count",
+                "pill_count_not_performed_reason",
+            ),
+        },
+    )
 
 
 def get_visual_score_fieldset_tuple(
     description: str | None = None, section_title: str | None = None
 ):
     section_title = section_title or _("Visual Score")
+    description = description or render_to_string(
+        "edc_adherence/visual_score_fieldset_description.html"
+    )
     return (
         section_title,
         {
-            "description": format_html(
-                description
-                or render_to_string("edc_adherence/visual_score_fieldset_description.html")
-            ),
+            "description": format_html(description),
             "fields": ("visual_score_slider", "visual_score_confirmed"),
         },
     )
@@ -57,11 +64,11 @@ class MedicationAdherenceAdminMixin:
     fieldsets = (
         (None, {"fields": ("subject_visit", "report_datetime")}),
         get_visual_score_fieldset_tuple(),
-        pill_count_fieldset_tuple,
-        missed_medications_fieldset_tuple,
+        get_pill_count_fieldset_tuple(),
+        get_missed_medications_fieldset_tuple(),
         audit_fieldset_tuple,
     )
 
-    radio_fields = {"last_missed_pill": admin.VERTICAL}
+    radio_fields = {"pill_count_performed": admin.VERTICAL, "last_missed_pill": admin.VERTICAL}
 
     filter_horizontal = ("missed_pill_reason",)
